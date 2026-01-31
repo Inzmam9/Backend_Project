0c64d0efa8d7cf4957b9ca3dd245f0108b68642b
Inzmam9 <143924063+Inzmam9@users.noreply.github.com>
2026-01-31 12:38:55 +0500

    Update Async_Handler.js

--- src/utils/Async_Handler.js
+++ src/utils/Async_Handler.js
@@ -1,14 +1,4 @@
-// const asyncHandler=(asyncHandles)=>async (req, res, next)=>{
-//     try{
-//         await asyncHandles(req, res, next)
-//     }
-//     catch(error){
-//         res.status(err.code||500).json({
-//             success:false,
-//             message:err.message
-//         })
-//     }
-// }
+
 
 const asyncHandler=(asyncHandles)=>(req, res, next)=>{
   
