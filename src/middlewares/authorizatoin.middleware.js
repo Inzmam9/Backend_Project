16e43fa5ee444bde598d604280eafe6c9275a670
Inzmam9 <143924063+Inzmam9@users.noreply.github.com>
2026-01-31 12:36:53 +0500

    Update authorizatoin.middleware.js

--- src/middlewares/authorizatoin.middleware.js
+++ src/middlewares/authorizatoin.middleware.js
@@ -1,8 +1,5 @@
 import asyncHandler from "../utils/Async_Handler.js";
 import { User } from "../models/user.model.js";
-//import jwt, { verify } from "jsonwebtoken"
-//import jwt from "jsonwebtoken"
-//import pkg from 'jsonwebtoken';
 import { ApiResponse } from "../utils/API_Response.js";
 import ApiError from "../utils/API_Error.js";
 //import { use } from "react";
@@ -14,46 +11,6 @@ export const verify = jwt.verify;
 
 
 
-
- //const authorization=asyncHandler(async(req,res,next)=>{
-// 
-//try {
-    
-//     req.cookies?.accessToken||req.header("Authorization")?.replace("Bearer ","")
-//     if(!req.cookies?.accessToken||req.header("Authorization")?.replace("Bearer ","")){
-//         throw new ApiError(404,"Token not found")
-    
-//     }
-//     const token= await req.cookies?.accessToken||req.header("Authorization")?.replace("Bearer ","")
-    
-    
-//     const verifyUser=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-//     if(!verifyUser){
-//          throw new ApiError(500,"User not verified")
-//     }
-    
-    
-//     const user=await User.findById(verifyUser._id).select("-password -refreshTokn")
-    
-//     if(!use){
-//         throw new ApiError(500, "Error in finding user")
-//     }
-    
-//     req.user=user
-    
-//     next()
-    
-    
-    
-// } catch (error) {
-//     throw new ApiError(404,"")
-// }
-
-// })
-
-
-
-// 
 
 const authorization=async(req, _, next)=>
 
